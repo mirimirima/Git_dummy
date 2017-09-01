71538ef660cc0f19ebc448b8bc1172677bcf91b5
Stefano Di Lorenzo <stefano.dilorenzo@gssi.it>
2017-09-01 10:14:28 +0200

    areaTri added to branchSte

---
+++ stefano/areaTri.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+void main(){
+  
+  int base,altezza,area;
+  
+  printf("Inserire: base altezza\n");
+  
+  scanf("%d %d",&base,&altezza); 
+  
+  area = base * altezza/2;
+
+  return area;
+}
